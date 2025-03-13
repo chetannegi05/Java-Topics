2324de0135aa4919c3b31a98a24b0acb92e7bda4
Chetan Negi <chetannegi574@gmail.com>
2025-03-13 23:48:04 +0530

    .\LinkList\LinkListPerformance.java

--- LinkList/LinkListPerformance.java
+++ LinkList/LinkListPerformance.java
@@ -73,13 +73,37 @@ public class LinkListPerformance {
             tail = newNode;
         }
     }
+    void addAtMiddle(int pos,int data)
+    {
+        int count=0;
+        Node newNode = new Node(data);
+        Node prev = head;
+        if(head == null)
+        {
+            System.out.println("List is empty");
+        }
+        else if(pos == 0)
+        {
+            addFirst(data);
+        }
+        else
+        {
+            while(count != (pos-1))
+            {
+                prev = prev.next;
+                count++;
+            }
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+    }
     public static void main(String[] args)
     {
         int data,ch;
         LinkListPerformance ll = new LinkListPerformance();
         Scanner sc = new Scanner(System.in);
         do {
-            System.out.print("Enter 1 for insert\nEnter 2 for Display\nEnter 3 for Delete\nEnter 4 for lastInsert\nEnter 5 for break\n");
+            System.out.print("Enter 1 for insert\nEnter 2 for Display\nEnter 3 for Delete\nEnter 4 for lastInsert\nEnter 5 for insert in pos\nEnter 6 for break\n");
             ch = sc.nextInt();
             switch (ch)
             {
@@ -109,6 +133,16 @@ public class LinkListPerformance {
                 }
                 case 5:
                 {
+                    int pos;
+                    System.out.println("Enter the element to insert");
+                    data = sc.nextInt();
+                    System.out.println("Enter the position to insert");
+                    pos = sc.nextInt();
+                    ll.addAtMiddle( pos, data);
+                    break;
+                }
+                case 6:
+                {
                     break;
                 }
                 default:
@@ -116,6 +150,6 @@ public class LinkListPerformance {
                     System.out.println("Enter valid choice");
                 }
             }
-        }while (ch!=5);
+        }while (ch!=6);
     }
 }
