37a45cf35806b6fa7b3c041d0ef840de4feb1c85
Chetan Negi <chetannegi574@gmail.com>
2025-03-15 11:28:47 +0530

    .\LinkList\LinkListPerformance.java

--- LinkList/LinkListPerformance.java
+++ LinkList/LinkListPerformance.java
@@ -13,11 +13,13 @@ public class LinkListPerformance {
             this.next = null;
         }
     }
-    Node head;
-    Node tail;
+    public static Node head;
+    public static Node tail;
+    public static int size;
     void addFirst(int data)
     {
         Node newNode = new Node(data);
+        size++;
         if(head == null)
         {
             head = tail = newNode;
@@ -48,21 +50,29 @@ public class LinkListPerformance {
     }
     void removeFirst()
     {
-        Node q = head;
+//        Node q = head;
         if(head == null)
         {
             System.out.println("List is empty");
         }
+        else if(size == 1)
+        {
+            System.out.println(head.data);
+            head = tail = null;
+            size = 0;
+        }
         else
         {
-            System.out.println("Deletion element is "+ q.data);
-            head = q.next;
+            System.out.println("Deletion element is "+ head.data);
+            head = head.next;
+            size--;
         }
     }
     void addLast(int data)
     {
         Node newNode = new Node(data);
         Node q = head;
+        size++;
         if(head == null)
         {
             head = tail = newNode;
@@ -86,6 +96,10 @@ public class LinkListPerformance {
         {
             addFirst(data);
         }
+        else if (pos == size)
+        {
+            addLast(data);
+        }
         else
         {
             while(count != (pos-1))
@@ -95,15 +109,70 @@ public class LinkListPerformance {
             }
             newNode.next = prev.next;
             prev.next = newNode;
+            size++;
         }
+    }
+    void removeLast()
+    {
+        Node q = head;
+        if(head == null)
+        {
+            System.out.println("List is empty");
+        }
+        else if(size == 1)
+        {
+            System.out.println(head.data);
+            head = tail = null;
+            size = 0;
+        }
+        else
+        {
+            System.out.println("Deletion element is "+ tail.data);
+            while(q.next.next != null)
+            {
+                q = q.next;
+            }
+            q.next = null;
+            tail = q;
+            size--;
+        }
+    }
+    int search(int key)
+    {
+        Node q = head;
+        int i = 0;
+        while(q!=null)
+        {
+            if(q.data == key)
+            {
+                return i;
+            }
+            q = q.next;
+            i++;
+        }
+        return -1;
+    }
+    void reverse() //TimeComplexity(0(n))
+    {
+        Node prev = null;
+        Node curr = tail = head;
+        Node nnext;
+        while(curr!=null)
+        {
+            nnext = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nnext;
+        }
+        head = prev;
     }
     public static void main(String[] args)
     {
-        int data,ch;
+        int data,ch,key;
         LinkListPerformance ll = new LinkListPerformance();
         Scanner sc = new Scanner(System.in);
         do {
-            System.out.print("Enter 1 for insert\nEnter 2 for Display\nEnter 3 for Delete\nEnter 4 for lastInsert\nEnter 5 for insert in pos\nEnter 6 for break\n");
+            System.out.print("Enter 1 for insert\nEnter 2 for Display\nEnter 3 for Delete\nEnter 4 for lastInsert\nEnter 5 for insert in pos\nEnter 6 for size of LinkedList\nEnter 7 for RemoveLast\nEnter 8 for search\nEnter 9 for Reverse\nEnter 10 for break\n");
             ch = sc.nextInt();
             switch (ch)
             {
@@ -143,6 +212,34 @@ public class LinkListPerformance {
                 }
                 case 6:
                 {
+                    System.out.println("Size of LinkedList " + ll.size);
+                    break;
+                }
+                case 7:
+                {
+                    ll.removeLast();
+                    break;
+                }
+                case 8:
+                {
+                    System.out.println("Enter the element to check");
+                    key = sc.nextInt();
+                    int ans = ll.search(key);
+                    if(ans == -1)
+                    {
+                        System.out.println("Element not found");
+                        break;
+                    }
+                    System.out.println("Element is present at index "+ ans);
+                    break;
+                }
+                case 9:
+                {
+                    ll.reverse();
+                    break;
+                }
+                case 10:
+                {
                     break;
                 }
                 default:
@@ -150,6 +247,6 @@ public class LinkListPerformance {
                     System.out.println("Enter valid choice");
                 }
             }
-        }while (ch!=6);
+        }while (ch!=10);
     }
 }
